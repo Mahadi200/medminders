2265bf25aa28760f6d2662a0fa23d5cc64d7ba4b
Mahadi200 <shurov895@gmail.com>
2025-07-16 00:55:12 +0600

    "Fix: Correct import path for DoctorLayout to match case"

--- src/components/home/Doctor/DocLayout.jsx
+++ src/components/home/Doctor/DocLayout.jsx
@@ -1,25 +1,22 @@
-import React from 'react'
-import DocDashboard from './DocDashboard'
-import DoctorLayout from './doctorlayout'
-import DashboardContent from './DashboardContent'
-import Navbar from '../Navbar'
-import Footer from '../Footer'
-
+import React from 'react';
+import DocDashboard from './DocDashboard';
+import DoctorLayout from './DoctorLayout'; // ✅ Corrected casing here
+import DashboardContent from './DashboardContent';
+import Navbar from '../Navbar';
+import Footer from '../Footer';
 
 const DocLayout = () => {
   return (
     <div>
-<div className="min-h-screen flex flex-col ">
-    <Navbar/>
-        {/* <DocDashboard/> */}
-        <DoctorLayout/>
-        <DashboardContent/>
-        <Footer/>
-
-       </div>
-      
+      <div className="min-h-screen flex flex-col">
+        <Navbar />
+        {/* <DocDashboard /> */}
+        <DoctorLayout />
+        <DashboardContent />
+        <Footer />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default DocLayout
+export default DocLayout;
